34309315d5df649b9f5dbb1534c74c98a70fb28f
MariMari021 <mariana.gsilva0021@gmail.com>
2024-04-17 16:33:50 -0300

    okok

---
+++ primeiroProjeto/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   /*
+            Console.WriteLine("Olá Mundo");
+
+            int idade = 23;
+            Console.WriteLine("A minha idade é: " + idade);
+            */
+
+
+            /*OPERAÇÕES MATEMÁTICAS
+            int numero1 = 9;
+            int numero2 = 3;
+
+            int soma = numero1 + numero2;
+            int subtracao = numero1 - numero2;
+            int multiplicacao = numero1 * numero2;
+            int divisao = numero1 / numero2;
+
+            Console.WriteLine(divisao);
+            Console.WriteLine(multiplicacao);
+            Console.WriteLine(subtracao);
+            Console.WriteLine(soma);
+
+            double salario = 1500.86;
+            Console.WriteLine(salario);*/
+
+            /*Atividade Um
+
+            int horas = 8;
+            int salario = 35;
+
+            int multiplicacao = salario * horas;
+            Console.WriteLine("Seu salário em um dia será " + multiplicacao + " reais.");*/
+
+            /*Atividade Dois
+
+            int caixa = 27;
+            int bombom = 16;
+
+            int bombons = caixa * bombom;
+
+            Console.WriteLine("Pamela terá " + bombons + " bombons");*/
+
+            /*Atividade Tres
+
+            int valorUm = 5;
+            int valorDois = 36;
+
+            int soma = valorDois + valorUm;
+
+            Console.WriteLine("A soma dos dois valores lidos é " + soma);*/
+
+            /*Atividade Quatro
+
+            int garrafas = 300;
+            int caixas = 20;
+
+            int divisao = garrafas / caixas;
+            Console.WriteLine("Eles usaram " + divisao + " para armazenar 300 garrafas.");*/
+
+            /* Atividade Cinco
+
+            int instalacao = 300;
+            int limpeza = 150;
+            double manutencao = 499.99;
+            int desconto = 50;
+
+            double soma = instalacao + limpeza + manutencao - desconto;
+            Console.WriteLine("Novos clientes deveram pagar para a empresa R$ " + soma);*/
+
+
+
+
+        }
+    }
+}
